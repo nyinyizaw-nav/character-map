0b4cf593b0668cf0284d18f121ff43927219e4c7
Nyi Nyi Zaw <nyinyizaw.nav@gmail.com>
2021-01-27 23:20:16 +0630

    create express server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+let port = process.env.PORT
+
+if ( port == "" || port == undefined ) { 
+	port = 8000;
+}
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send(<h2>Hello From the Express</h2>)
+};
+
+app.listen(port, () => {
+	console.log(`Express is running on ${port}`);
+})
